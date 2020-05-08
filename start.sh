c9254f0bf932d2c869e152451bb9dc00403928a0
CorneliousJD <corneliousjd@gmail.com>
2020-05-08 14:03:12 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # if index.html doesn't exist yet copy all files
 if [ ! -f /usr/share/nginx/html/index.html ]; then
-  cp . /usr/share/nginx/html
+  cp -r /usr/share/holding /usr/share/nginx/html
 fi
