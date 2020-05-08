9e2be661cf93e1f0a3ba01a81cdca1527c64f243
CorneliousJD <corneliousjd@gmail.com>
2020-05-08 14:49:06 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+#check for existing index.html file, if exists leave alone, otherise copy default files.
 if [ ! -f /usr/share/nginx/html/index.html ]; then
 cp /usr/share/holding/*.* /usr/share/nginx/html
 cp /usr/share/holding/assets /usr/share/nginx/html
 fi
+#run nginx
 nginx -g "daemon off;"
